4425a79dc0789000fd620bed0242d9ce3ec0be71
SaimiAh <ah.saim786@gmail.com>
2022-08-09 20:45:02 +0500

    Changing Color

---
+++ myscript4.js
@@ -0,0 +1,21 @@
+var myheader =document.querySelector("h1")
+myheader.style.color="red"
+
+
+function getRandomColor(){
+   
+    var letters = "0123456789ABCDEF";
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random()*16)];
+    }
+    return color
+  }
+
+  function changeHeaderColor(){
+    colorInput = getRandomColor()
+    myheader.style.color = colorInput;
+  }
+  
+  // Now perform the action over intervals (milliseocnds):
+  setInterval("changeHeaderColor()",500);
